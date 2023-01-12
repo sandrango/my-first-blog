1b2add0b68ef6d0d2749d63b58876e66f374cf39
Stalyn <sandrango@manamer.com>
2023-01-12 11:57:44 -0500

    Cambie los static para la pagina

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 
 
@@ -11,6 +12,7 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
-
-    

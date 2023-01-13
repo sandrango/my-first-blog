06d48382a4c64442349cddbeeeaaa02e1ada931c
sandrango <sandrango@manamer.com>
2023-01-13 10:00:33 -0500

    LOCAL TEST2

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 
 class Post(models.Model):
     #STALYN LOCAL
+    #TEST2
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()

f4e0bf14a932e120ab36ed2f96ea209ac522b1db
sandrango <sandrango@manamer.com>
2023-01-13 09:53:26 -0500

    Act locales stalyn

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    #STALYN LOCAL
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
